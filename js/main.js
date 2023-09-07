4c4e92c506151485ed8d2c46d9df4ec05acaaf6b
Marat_Rakhimov <morris260781@gmail.com>
2023-09-07 23:05:05 +0500

    edition_07.09.2023

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoader', function(){
+// показать/скрыть мобильное меню
+    const menuToggle = document.querySelector('#header-top__mobile-icon');
+    const headerMenu = document.querySelector('#header-top__nav');
+    // прослушивание клика по кнопке
+    menuToggle.addEventListener('click', function(){
+        if(this.classList.contains('active')){
+            this.classList.remove('active');
+        }
+        else{
+            this.classList.add('active');
+        }
+    });
+});
