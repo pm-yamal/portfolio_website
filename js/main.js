f5f109e7cd5461691a8ea690ea7f93698d7432d3
Marat_Rakhimov <morris260781@gmail.com>
2023-09-14 22:02:43 +0500

    edition_14.09.2023

--- js/main.js
+++ js/main.js
@@ -3,16 +3,27 @@
 // })
 
 document.addEventListener('DOMContentLoaded', function(){
-// показать/скрыть мобильное меню
+// показать/скрыть мобильное меню, зафиксировать body
     const menuToggle = document.querySelector('#toggle');
     const headerMenu = document.querySelector('#header-top__menu');
+    const bodyLock = document.body;
     // прослушивание клика по кнопке
     menuToggle.addEventListener('click', function(){
         if(this.classList.contains('toggle--active')){
             this.classList.remove('toggle--active');
+            headerMenu.classList.remove('header-top__menu--open');
+            bodyLock.classList.remove('body--lock');
         }
         else{
             this.classList.add('toggle--active');
+            headerMenu.classList.add('header-top__menu--open');
+            bodyLock.classList.add('body--lock');
         }
+    });
+
+    headerMenu.addEventListener('click', function(){
+        this.classList.remove('header-top__menu--open');
+        menuToggle.classList.remove('toggle--active');
+        bodyLock.classList.remove('body--lock');
     });
 });
